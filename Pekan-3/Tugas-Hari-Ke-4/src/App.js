bdcc8a8948bf929a4ec650a446945ff10ec1e5aa
ariefrino50 <53212851+ariefrino50@users.noreply.github.com>
2020-08-08 08:17:25 +0700

    Add files via upload

---
+++ Pekan-3/Tugas-Hari-Ke-4/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import  FromBuahHooks from './tugas14/Hook';
+
+function App() {
+  return (
+    <div className="App">
+     {/* TUGAS 11*/}
+        {/*<Tugas11 />*/}
+
+        {/* TUGAS 12 
+        <Jam/>*/}
+        {/* TUGAS 13
+        <DaftarBuah/>*/}
+        <FromBuahHooks/>
+       
+    </div>
+  );
+}
+
+export default App;
